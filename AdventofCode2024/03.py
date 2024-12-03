4f9e82ffeb14907575f774d60961b2e1d82d178f
simikraemer <simonkraemer@weh.rwth-aachen.de>
2024-12-03 07:01:42 +0100

    03

---
+++ AdventofCode2024/03.py
@@ -0,0 +1,23 @@
+import re
+
+with open("AdventofCode2024/input/03.txt", "r") as file:
+    input = file.read()
+
+pattern = r"(do\(\)|don't\(\)|mul\((\d+),(\d+)\))"
+funktionen = re.findall(pattern, input)
+aufgabe1 = 0
+aufgabe2 = 0
+enabled = True
+
+for funktion, x, y in funktionen:
+    if funktion == "do()":
+        enabled = True
+    elif funktion == "don't()":
+        enabled = False
+    elif funktion.startswith("mul"):
+        aufgabe1 += int(x) * int(y)
+        if enabled:
+            aufgabe2 += int(x) * int(y)
+
+print(aufgabe1)
+print(aufgabe2)
