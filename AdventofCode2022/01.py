41f3ed8cb654d06d1446bbd9386b7171cd969dc0
simikraemer <simonkraemer@weh.rwth-aachen.de>
2025-09-17 13:03:40 +0200

    2022_01

---
+++ AdventofCode2022/01.py
@@ -0,0 +1,16 @@
+with open("AdventofCode2022/input/01.txt", "r") as file:
+    inhalt = file.read().strip()
+    elfen = inhalt.split("\n\n")
+
+summen = []
+for elf in elfen:
+    zeilen = elf.strip().split("\n")
+    kalorien = [int(zeile) for zeile in zeilen]
+    summen.append(sum(kalorien))
+
+x1 = max(summen)
+top_drei = sorted(summen, reverse=True)[:3]
+x2 = sum(top_drei)
+
+print(x1)
+print(x2)
