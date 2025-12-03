8c48fb7235bd2c14cac3bbc2f2d4ffd09de395a6
simikraemer <simonkraemer@weh.rwth-aachen.de>
2025-12-03 08:40:33 +0100

    2025_3.2

--- 2025/03.py
+++ 2025/03.py
@@ -1,7 +1,7 @@
 maxjoltages = []
 
 with open("2025/input/03.txt", "r") as file:
-    lines = file.readlines()    
+    lines = file.readlines()
     
 for line in lines:
     line = line.strip()
@@ -17,11 +17,42 @@ for line in lines:
             stelle2 = int(ziffer)
     
     maxjoltage = int(str(stelle1) + str(stelle2))
-    print(maxjoltage)
     maxjoltages.append(maxjoltage)
         
 aufgabe1 = sum(maxjoltages)
-aufgabe2 = 0
+
+
+def findmaxnumber(offenestellen, line):
+    stelle = 0
+    restline = ""
+    if offenestellen > 0:
+        restline = str(line)[:-offenestellen]
+    else:
+        restline = str(line)
+    for i,ziffer in enumerate(restline):
+        if int(ziffer) > stelle:
+            stelle = int(ziffer)
+            position = i
+    restline = str(line)[position + 1:]
+    return stelle, restline
+
+maxjoltages = []
+
+for line in lines:
+    line = line.strip()
+    offenestellen = 11
+    stellen = []
+
+    while offenestellen > -1:
+        stelle, restline = findmaxnumber(offenestellen, line)
+        line = restline
+        stellen.append(stelle)
+        offenestellen -= 1
+
+    maxjoltage = int("".join(str(stelle) for stelle in stellen))
+    maxjoltages.append(maxjoltage)
+
+aufgabe2 = sum(maxjoltages)
 
 print(f"Aufgabe 1: {aufgabe1}")
 print(f"Aufgabe 2: {aufgabe2}")
