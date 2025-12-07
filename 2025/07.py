72aeb0c09930a11c7ceee1ef88b4407b2d3d5d62
simikraemer <simonkraemer@weh.rwth-aachen.de>
2025-12-07 13:25:49 +0100

    7.2.

--- 2025/07.py
+++ 2025/07.py
@@ -5,29 +5,29 @@ with open(Path(__file__).resolve().parent / "input" / "07.txt", "r") as file:
     lines = file.readlines()
 for y, line in enumerate(lines):
     line = line.strip()
-    höhe = len(lines)
     breite = len(line)
     linearray = []
+    leerzeile = True
     for x, eintrag in enumerate(line):
         linearray.append(eintrag)
         if eintrag == "S":
+            leerzeile = False
             start = (x,y)
         if eintrag == "^":
+            leerzeile = False
             splitter.append((x,y))
-    matrix.append(linearray)
-#print(start)
-#print(splitter)
-#print(breite)
-#print(höhe)
+    if not leerzeile:
+        matrix.append(linearray)
+
+höhe = len(matrix)
 
 aufgabe1 = 0
-
 takyon = []
 takyon.append(start)
 for y, line in enumerate(matrix):
     for x, eintrag in enumerate(line):
         if (x,y) in takyon:
-            if y == höhe - 1:
+            if y == höhe-1:
                 break
             if matrix[y+1][x] != "^":
                 if (x,y+1) not in takyon:
@@ -39,11 +39,27 @@ for y, line in enumerate(matrix):
                 if (x+1,y+1) not in takyon:
                     takyon.append((x+1,y+1))
             
-aufgabe2 = 0
-
-        
-print(takyon)
-            
+aufgabe2 = 1
+takyon = {}
+takyon[start] = 1
+for y, line in enumerate(matrix):
+    for x, eintrag in enumerate(line):
+        if (x,y) in takyon:
+            anzahl = takyon[(x, y)]
+            if y == höhe-1:
+                break
+            if matrix[y+1][x] != "^":
+                if ((x,y+1) not in takyon):
+                    takyon[(x,y+1)] = 0
+                takyon[(x,y+1)] += anzahl
+            else:
+                aufgabe2 += anzahl
+                if ((x-1,y+1) not in takyon):
+                    takyon[(x-1,y+1)] = 0
+                if ((x+1,y+1) not in takyon):
+                    takyon[(x+1,y+1)] = 0
+                takyon[(x-1,y+1)] += anzahl
+                takyon[(x+1,y+1)] += anzahl           
     
 print(f"Aufgabe 1: {aufgabe1}")
 print(f"Aufgabe 2: {aufgabe2}")
